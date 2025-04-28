1b2c56dfef3edcb93da55db766dbf7f7722fdd6b
andrewfulton9 <andrewfulton9@gmail.com>
2025-04-28 12:21:51 -0600

    remove license symlink

--- package_build/initialize.sh
+++ package_build/initialize.sh
@@ -29,7 +29,6 @@ do
   ln -sf $BASEDIR/tfx $BASEDIR/package_build/$CONFIG_NAME/
   ln -sf $BASEDIR/MANIFEST.in $BASEDIR/package_build/$CONFIG_NAME/
   ln -sf $BASEDIR/README*.md $BASEDIR/package_build/$CONFIG_NAME/
-  ln -sf $BASEDIR/LICENSE $BASEDIR/package_build/$CONFIG_NAME/
 
   rm -rf $BASEDIR/package_build/$CONFIG_NAME/build
   mkdir $BASEDIR/package_build/$CONFIG_NAME/build
