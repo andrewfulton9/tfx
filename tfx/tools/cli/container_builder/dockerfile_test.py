a299485122831211324243980d3acf386030b165
andrewfulton9 <andrewfulton9@gmail.com>
2025-07-02 13:39:08 -0600

    fix test try

--- tfx/tools/cli/container_builder/dockerfile_test.py
+++ tfx/tools/cli/container_builder/dockerfile_test.py
@@ -67,7 +67,7 @@ class DockerfileTest(test_case_utils.TfxTest):
         base_image='my_customized_image:latest')
     self.assertTrue(
         filecmp.cmp(
-            os.path.join(self._testdata_dir, 'test_dockerfile_with_base'),
+            os.path.join(self._testdata_dir, 'test_dockerfile_with_base', shallow=False),
             generated_dockerfile_path))
 
   def testDevVersionRequirement(self):
